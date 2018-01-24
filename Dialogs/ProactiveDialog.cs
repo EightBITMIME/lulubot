e6d25f9c8d6750900e53c3f69b0069a8490b8c50
Hutchison <lhutchis@txt.textron.com>
2018-01-24 16:31:01 -0600

    describe incident part 2

--- Dialogs/ProactiveDialog.cs
+++ Dialogs/ProactiveDialog.cs
@@ -34,13 +34,16 @@ namespace Microsoft.Bot.Sample.ProactiveBot
                 // write the queue Message to the queue
                 await AddMessageToQueueAsync(JsonConvert.SerializeObject(queueMessage));
 
-            if(message.Text == "I would like to report a security incident")
+            if (message.Text == "I would like to report a security incident")
             {
                 await context.PostAsync($"Please describe the security incident");
                 context.Wait(MessageReceivedAsync);
             }
+            else
+            {
                 await context.PostAsync($"<sarcastic mocking> {queueMessage.Text}.");
                 context.Wait(MessageReceivedAsync);
+            }
         }
 
         public static async Task AddMessageToQueueAsync(string message)
