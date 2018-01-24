475d38eb2dfc1c702fe949fd85e9dd7eaeaea427
Cyr <dcyr01@txt.textron.com>
2018-01-24 15:35:33 -0600

    Initial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.ProactiveBot.sln -p:DeployOnBuild=true -p:PublishProfile=luusecuritybot-Web-Deploy.pubxml -p:Password=0vJ29rf01oCwBi7ng43PlzCrbiWm4F9oyGGf6LGAc1Pl6PgzkbDFX5aTEGpk
+
